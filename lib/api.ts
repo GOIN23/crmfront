b1e00fc150066f7ae02dd82c37f2230e459d5ef2
Ali <4e5.kn@mail.ru>
2026-03-18 15:24:08 +0300

    fix: refactoring

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://194.5.79.68:30532/api/v1',
   withCredentials: true,
 });
 
